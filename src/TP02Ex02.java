b004f9fa1ea1efe3a15dc1a1de426f23f9934505
Guilherme Pontes <guilherme-pontes2015@hotmail.com>
2022-04-29 11:26:08 -0300

    ex02

--- src/TP02Ex02.java
+++ src/TP02Ex02.java
@@ -12,18 +12,19 @@ public class TP02Ex02 {
     public static void main(String[] args){
         Scanner ler = new Scanner(System.in);
 
-        double maior, soma = 0, num, media;
+        double maior = 0, soma = 0, num, media, num1;
 
         out.print("Informe o 1º número: ");
-        maior = ler.nextInt();
-
+        num1 = ler.nextInt();
+        soma += num1;
         for (int i = 2; i <= 10; i++) {
+
             out.print("Informe o " + i + "º número: ");
             num = ler.nextInt();
 
             soma += num;
 
-            if(num > maior)
+            if(num > num1)
                 maior = num;
         }
 
