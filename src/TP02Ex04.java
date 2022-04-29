f2e96ee6c25959fa9e62a7de7addc3c3b7783351
Guilherme Pontes <guilherme-pontes2015@hotmail.com>
2022-04-29 12:09:13 -0300

    ex04

---
+++ src/TP02Ex04.java
@@ -0,0 +1,27 @@
+//4. Armazenar seis valores em uma matriz de ordem 2x3. Apresentar os valores na tela.
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class TP02Ex04 {
+
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+
+        int[][] v = new int[2][3];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.printf("Informe [" + i + "][" + j + "]: ");
+                v[i][j] = ler.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.print("[" + v[i][j] + "] ");
+            }
+            out.println();
+        }
+    }
+}
